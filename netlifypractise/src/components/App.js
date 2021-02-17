17a04426141ac6ea3bb43104c8dcb6d3f1e28ac1
ilker <ilkerpeker79@yahoo.com>
2021-02-17 16:24:16 +0000

    stuck

--- netlifypractise/src/components/App.js
+++ netlifypractise/src/components/App.js
@@ -1,20 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react'
 
 
 function App() {
   
-const [Starwars, setStarwars] = useState('')
+const [Starwars, setStarwars] = useState([])
 
   useEffect(() => {
     async function getStarwars(){
-      // let response = await fetch(process.env.REACT_APP_API_URL)
-      let response = await fetch("https://swapi.dev/api/people?")
+      let response = await fetch(process.env.REACT_APP_API_URL)
+      // let response = await fetch("https://swapi.dev/api/people?")
       let data = await response.json()
       setStarwars(data.results)
-      console.log(data.results[1].name)
+      console.log(data)
     }
     getStarwars()
-  },)
+  },[])
   
   return (
     <div>
